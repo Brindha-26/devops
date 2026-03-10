2304aeb04055fd31bfadfb338bbf52bc4f0e12b4
Brindha <brindham2608@gmail.com>
2026-03-10 11:55:17 +0530

    sample

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,17 +1,13 @@
-import java.util.Scanner;
-
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int num1 = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int num2 = sc.nextInt();
+        int num1 = 5;
+        int num2 = 10;
 
         int sum = num1 + num2;
 
+        System.out.println("First Number: " + num1);
+        System.out.println("Second Number: " + num2);
         System.out.println("Sum = " + sum);
     }
 }
